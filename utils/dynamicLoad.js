9349bbabc0babf76690f1150310ce9a66db78c16
Tommaso Bossi <tommaso.bossi@forge.srl>
2022-01-13 12:15:34 +0100

    ci: jest coverage

--- utils/dynamicLoad.js
+++ utils/dynamicLoad.js
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 // Because CJS and ESM hate each other...
 // https://redfin.engineering/node-modules-at-war-why-commonjs-and-es-modules-cant-get-along-9617135eeca1
 // We (and jest) have chosen the CJS side, but we use some ESM modules and ESM support in jest sucks (see:
