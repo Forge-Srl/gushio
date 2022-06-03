e6043b21bde9e4d5505cc06572e3f8c0d380736c
Tommaso Bossi <tommaso.bossi@forge.srl>
2022-06-03 08:27:10 +0200

    fix: fetchWrapper on Node 18
    Starting with Node.js 18, `fetch` is already available as a global function.

--- runner/patches/fetchWrapper.js
+++ runner/patches/fetchWrapper.js
@@ -2,12 +2,17 @@ import {FunctionWrapper} from './FunctionWrapper.js'
 import {default as nodeFetch} from 'node-fetch'
 
 export const fetchWrapper = () => {
+    // Global fetch is available from Node 18
     const originalFetch = global.fetch
     const before = () => {
-        global.fetch = nodeFetch
+        if (!originalFetch) {
+            global.fetch = nodeFetch
+        }
     }
     const after = () => {
-        global.fetch = originalFetch
+        if (!originalFetch) {
+            global.fetch = originalFetch
+        }
     }
 
     return new FunctionWrapper(before, after)
--- tests/runner/patches/fetchWrapper.test.js
+++ tests/runner/patches/fetchWrapper.test.js
@@ -1,4 +1,5 @@
 import {jest, describe, test, beforeAll, beforeEach, afterEach, afterAll, expect} from '@jest/globals'
+import semver from 'semver'
 
 describe('fetchWrapper', () => {
     let fetchWrapper, fetch
@@ -9,16 +10,30 @@ describe('fetchWrapper', () => {
         fetchWrapper = (await import('../../../runner/patches/fetchWrapper.js')).fetchWrapper
     })
 
-    test('fetchWrapper', async () => {
-        fetch.mockImplementationOnce(() => 'httpResult')
-        const fn = async () => {
-            expect(await global.fetch('url', 'settings')).toBe('httpResult')
-            return 'something'
-        }
-        expect(global.fetch).toBeUndefined()
-        expect(await fetchWrapper().run(fn)).toBe('something')
-        expect(fetch).toHaveBeenCalledTimes(1)
-        expect(fetch).toHaveBeenCalledWith('url', 'settings')
-        expect(global.fetch).toBeUndefined()
-    })
+    if (semver.satisfies(process.versions.node, '<18')) {
+        test('fetchWrapper before Node 18', async () => {
+            fetch.mockImplementationOnce(() => 'httpResult')
+            const fn = async () => {
+                expect(await global.fetch('url', 'settings')).toBe('httpResult')
+                return 'something'
+            }
+            expect(global.fetch).toBeUndefined()
+            expect(await fetchWrapper().run(fn)).toBe('something')
+            expect(fetch).toHaveBeenCalledTimes(1)
+            expect(fetch).toHaveBeenCalledWith('url', 'settings')
+            expect(global.fetch).toBeUndefined()
+        })
+    } else {
+        test('fetchWrapper from Node 18', async () => {
+            const capturedFetch = global.fetch
+            const fn = async () => {
+                expect(global.fetch).toBe(capturedFetch)
+                return 'something'
+            }
+            expect(global.fetch).not.toBeUndefined()
+            expect(await fetchWrapper().run(fn)).toBe('something')
+            expect(fetch).not.toHaveBeenCalled()
+            expect(global.fetch).toBe(capturedFetch)
+        })
+    }
 })
